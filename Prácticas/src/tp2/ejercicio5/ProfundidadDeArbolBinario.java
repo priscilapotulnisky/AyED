818c314c5c5b1b7697cbb268619c8162a2781057
priscilapotulnisky <potulniskyp@gmail.com>
2025-04-06 16:59:57 -0300

    Ejercicio 5

--- Prácticas/src/tp2/ejercicio5/ProfundidadDeArbolBinario.java
+++ Prácticas/src/tp2/ejercicio5/ProfundidadDeArbolBinario.java
@@ -1,6 +1,39 @@
 package tp2.ejercicio5;
 import tp2.ejercicio1.*;
+import tp1.ejercicio8.*;
 public class ProfundidadDeArbolBinario {
+
+	private BinaryTree<Integer> arbol;
+	
+	public ProfundidadDeArbolBinario(BinaryTree<Integer> arbol) {
+		this.arbol = arbol;
+	}
+	public int sumaElementosProfundidad(int p) {
+		int nivel = 0;
+		if(arbol.isEmpty())
+			return 0;
+		int suma = 0;
+		Queue<BinaryTree<Integer>> cola = new Queue<>();
+		BinaryTree<Integer> arbol;
+		cola.enqueue(this.arbol);
+		cola.enqueue(null);
+		while(!cola.isEmpty()) {
+			arbol = cola.dequeue();
+			if(arbol != null) {
+				if(nivel == p)
+					suma += arbol.getData();
+				if (arbol.hasLeftChild()) 
+					cola.enqueue(arbol.getLeftChild());
+				if (arbol.hasRightChild()) 
+					cola.enqueue(arbol.getRightChild());
+			}
+			else if (!cola.isEmpty()) {
+	   	    	cola.enqueue(null); 
+	   	    	nivel ++;
+			}
+		}
+		return suma;
+	}
 
 	
 }
---
+++ Prácticas/src/tp2/ejercicio5/Test.java
@@ -0,0 +1,28 @@
+package tp2.ejercicio5;
+
+import tp2.ejercicio1.BinaryTree;
+
+public class Test {
+
+	public static void main (String [] args) {
+		BinaryTree<Integer> arbol = new BinaryTree<>(10);
+		arbol.addLeftChild(new BinaryTree<Integer>(2));
+		arbol.getLeftChild().addLeftChild(new BinaryTree<Integer>(5));
+		arbol.getLeftChild().addRightChild(new BinaryTree<Integer>(4));
+		arbol.getLeftChild().getLeftChild().addLeftChild(new BinaryTree<Integer>(7));
+		arbol.getLeftChild().getLeftChild().addRightChild(new BinaryTree<Integer>(8));
+		arbol.getLeftChild().getRightChild().addLeftChild(new BinaryTree<Integer>(5));
+		arbol.getLeftChild().getRightChild().addRightChild(new BinaryTree<Integer>(6));
+		arbol.addRightChild(new BinaryTree<Integer>(3));
+		arbol.getRightChild().addLeftChild(new BinaryTree<Integer>(9));
+		arbol.getRightChild().getLeftChild().addLeftChild(new BinaryTree<Integer>(12));
+		arbol.getRightChild().getLeftChild().addRightChild(new BinaryTree<Integer>(8));
+		arbol.getRightChild().addRightChild(new BinaryTree<Integer>(8));
+		arbol.getRightChild().getRightChild().addLeftChild(new BinaryTree<Integer>(2));
+		arbol.getRightChild().getRightChild().addRightChild(new BinaryTree<Integer>(1));
+		
+		ProfundidadDeArbolBinario pdab = new ProfundidadDeArbolBinario(arbol);
+		int suma = pdab.sumaElementosProfundidad(1);
+		System.out.println(suma);
+	}
+}

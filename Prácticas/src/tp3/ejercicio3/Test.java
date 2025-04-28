01501b8f1d19e08af64d27c844b011153d9009ec
priscilapotulnisky <potulniskyp@gmail.com>
2025-04-28 17:50:12 -0300

    Ejercicio 3

---
+++ Prácticas/src/tp3/ejercicio3/Test.java
@@ -0,0 +1,44 @@
+package tp3.ejercicio3;
+
+import java.util.LinkedList;
+import java.util.List;
+
+//import tp3.ejercicio1.GeneralTree;
+
+public class Test {
+	public static void main(String[] args) {
+		// Crear los nodos
+	    GeneralTree<Integer> a1 = new GeneralTree<>(1); // Raíz
+	    GeneralTree<Integer> a2 = new GeneralTree<>(2); // Hijo de la raíz
+	    GeneralTree<Integer> a3 = new GeneralTree<>(31); // Hijo de la raíz
+	    GeneralTree<Integer> a4 = new GeneralTree<>(45); // Hijo de la raíz
+	    GeneralTree<Integer> a5 = new GeneralTree<>(29); // Hijo de a2
+	    GeneralTree<Integer> a6 = new GeneralTree<>(50); // Hijo de a2
+	    GeneralTree<Integer> a7 = new GeneralTree<>(123); // Hijo de a3
+	    GeneralTree<Integer> a8 = new GeneralTree<>(53); // Hijo de a3
+
+	    // Agregar hijos a los nodos correspondientes
+	    List<GeneralTree<Integer>> hijosRaiz = new LinkedList<>();
+	    hijosRaiz.add(a2);
+	    hijosRaiz.add(a3);
+	    hijosRaiz.add(a4);
+	    a1.setChildren(hijosRaiz);
+
+	    List<GeneralTree<Integer>> hijosA2 = new LinkedList<>();
+	    hijosA2.add(a5);
+	    hijosA2.add(a6);
+	    a2.setChildren(hijosA2);
+
+	    List<GeneralTree<Integer>> hijosA3 = new LinkedList<>();
+	    hijosA3.add(a7);
+	    hijosA3.add(a8);
+	    a3.setChildren(hijosA3);
+	    
+	    System.out.println(a1.preOrden());
+	    
+	    System.out.println(a1.altura());
+	    System.out.println(a1.nivel(2));
+	    System.out.println(a1.ancho());
+	   
+	}
+}
